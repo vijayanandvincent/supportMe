8e42675270f827c7dc2b81c903318f02f47468e6
vijayanandvincent <34533445+vijayanandvincent@users.noreply.github.com>
2025-12-23 10:52:58 +0530

    Update step1.sh

--- scenarios/cpu_high/steps/step1.sh
+++ scenarios/cpu_high/steps/step1.sh
@@ -3,6 +3,10 @@
 Run the following commands:
 
 ```bash
-top
-uptime
-mpstat
+top   - Real-time monitoring of system performance and identifying resource-consuming processes.
+-1.Now monitor for field name %CPU which is occupying more CPU% 
+-2.Notedown those PIDs. 
+-3.Then press Ctrl+c to exit
+uptime - System load & how long it’s been running
+mpstat - Per-CPU utilization statistics
+CLICK NEXT
