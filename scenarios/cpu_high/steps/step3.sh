cc8bc7dd1c6e474610b0ee9ba11a962fe62e8ff6
vijayanandvincent <34533445+vijayanandvincent@users.noreply.github.com>
2025-12-23 10:59:55 +0530

    Update step3.sh

--- scenarios/cpu_high/steps/step3.sh
+++ scenarios/cpu_high/steps/step3.sh
@@ -5,7 +5,10 @@
 ```md
 ## Step 3 – Fix the Issue
 
-Stop or reduce the process.
+Stop Or kill the process by using kill command
 
 ```bash
 kill -9 <PID>
+AGAIN RUN TOP TO CHECK PIDS BEEN KILLED AND %CPU HAS REDUCED BY USING BELOW COMMAND
+ps -eo pid,cmd,%cpu --sort=-%cpu | head
+NOW THE %CPU SHOULD HAVE BEEN REDUCED, IF NOT CONTINUE FROM STEP 1.
